58ab840d80e636b2f0b14c560fb76000d53c6bdc
Spencer Herms <slherms@gmail.com>
2017-11-01 16:10:11 -0600

    adds basic testing files

---
+++ test/CurrentWeather.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import CurrentWeather from '../lib/CurrentWeather';
+
+describe('CurrentWeather unit testing', () => {
+  it.skip('It should ', () => {
+    const component = shallow(<CurrentWeather />);
+
+
+  })
+
+})
---
+++ test/TenDay.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import TenDay from '../lib/TenDay';
+
+describe('TenDay unit testing', () => {
+  it.skip('It should ', () => {
+    const component = shallow(<TenDay />);
+
+
+  })
+
+})
